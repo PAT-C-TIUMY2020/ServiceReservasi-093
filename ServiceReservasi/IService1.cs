31b373ac50a5c98b2082c0b0c665f901c1b92221
Musafak <musafak.ft18@mail.umy.ac.id>
2020-12-12 15:59:26 +0700

    LOGIN DAN REGISTER

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using static ServiceReservasi.DataRegister;
 
 namespace ServiceReservasi
 {
@@ -30,6 +31,18 @@ namespace ServiceReservasi
         List<Pemesanan> Pemesanan();
         string GetData(int value);
 
+        //Login & Register
+        [OperationContract]
+        string Login(string username, string password);
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+        [OperationContract]
+        string DeleteRegister(string username);
+        [OperationContract]
+        List<DataRegister> DataRegist();
+
         //[OperationContract]
         //CompositeType GetDataUsingDataContract(CompositeType composite);
 
@@ -37,75 +50,89 @@ namespace ServiceReservasi
     }
 
     [DataContract]
-    public class CekLokasi //daftar lokasi nongrong
+    public class DataRegister
     {
-        [DataMember]
-        public string IDLokasi { get; set; } //variable dari public class
-
-        [DataMember]
-        public string NamaLokasi { get; set; }
-
-        [DataMember]
-        public string DeskripsiSingkat { get; set; }
-    }
-
-    [DataContract]
-    public class DetailLokasi //menampilkan detail lokasi
-    {
-        [DataMember]
-        public string IDLokasi { get; set; } //variable dari public class
-
-        [DataMember]
-        public string NamaLokasi { get; set; }
-
-        [DataMember]
-        public string DeskripsiFull { get; set; }
-
-        [DataMember]
-        public int Kouta { get; set; }
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
     }
 
 
-    [DataContract]
-    public class Pemesanan //crate
-    {
-        [DataMember]
-        public string IDPemesanan { get; set; } //variable dari public class
-
-        [DataMember]
-        public string NamaCustomer { get; set; } // method
-
-        [DataMember]
-        public string NoTelepon { get; set; }
-
-        [DataMember]
-        public int JumlahPemesanan { get; set; }
-
-        [DataMember]
-        public string Lokasi { get; set; }
-    }
-
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceReservasi.ContractType".
-    /*[DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
+        [DataContract]
+        public class CekLokasi //daftar lokasi nongrong
         {
-            get { return boolValue; }
-            set { boolValue = value; }
+            [DataMember]
+            public string IDLokasi { get; set; } //variable dari public class
+
+            [DataMember]
+            public string NamaLokasi { get; set; }
+
+            [DataMember]
+            public string DeskripsiSingkat { get; set; }
         }
 
-        [DataMember]
-        public string StringValue
+        [DataContract]
+        public class DetailLokasi //menampilkan detail lokasi
         {
-            get { return stringValue; }
-            set { stringValue = value; }
+            [DataMember]
+            public string IDLokasi { get; set; } //variable dari public class
+
+            [DataMember]
+            public string NamaLokasi { get; set; }
+
+            [DataMember]
+            public string DeskripsiFull { get; set; }
+
+            [DataMember]
+            public int Kouta { get; set; }
         }
-    }*/
-}
+
+
+        [DataContract]
+        public class Pemesanan //crate
+        {
+            [DataMember]
+            public string IDPemesanan { get; set; } //variable dari public class
+
+            [DataMember]
+            public string NamaCustomer { get; set; } // method
+
+            [DataMember]
+            public string NoTelepon { get; set; }
+
+            [DataMember]
+            public int JumlahPemesanan { get; set; }
+
+            [DataMember]
+            public string Lokasi { get; set; }
+        }
+
+
+        // Use a data contract as illustrated in the sample below to add composite types to service operations.
+        // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceReservasi.ContractType".
+        /*[DataContract]
+        public class CompositeType
+        {
+            bool boolValue = true;
+            string stringValue = "Hello ";
+
+            [DataMember]
+            public bool BoolValue
+            {
+                get { return boolValue; }
+                set { boolValue = value; }
+            }
+
+            [DataMember]
+            public string StringValue
+            {
+                get { return stringValue; }
+                set { stringValue = value; }
+            }
+        }*/
+    }
--- ServiceReservasi/Service1.cs
+++ ServiceReservasi/Service1.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using static ServiceReservasi.DataRegister;
 
 namespace ServiceReservasi
 {
@@ -15,6 +16,34 @@ namespace ServiceReservasi
         string constring = "Data Source=DESKTOP-5L26KM2;Initial Catalog=WCFReservasi;Persist Security Info=True;User ID=sa; Password=musafak93";
         SqlConnection connection;
         SqlCommand com; //untuk mengoneksi database ke visual studio
+
+        public List<DataRegister> DataRegist()
+        {
+            List<DataRegister> list = new List<DataRegister>();
+            try
+            {
+                string sql = "select ID_login, Username, Password, Kategori from Login";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    DataRegister data = new DataRegister();
+                    data.id = reader.GetInt32(0);
+                    data.username = reader.GetString(1);
+                    data.password = reader.GetString(2);
+                    data.kategori = reader.GetString(3);
+                    list.Add(data);
+                }
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+            return list;
+        }
 
         public string deletePemesanan(string IDPemesanan)
         {
@@ -34,6 +63,36 @@ namespace ServiceReservasi
                 Console.WriteLine(es);
             }
             return a;
+        }
+
+        public string DeleteRegister(string username)
+        {
+            try
+            {
+                int id = 0;
+                string sql = "select ID_login from dbo.Login where Username = '" + username + "'";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                SqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    id = reader.GetInt32(0);
+                }
+                connection.Close();
+                string sql2 = "delete from Login where ID_login" + id + "";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql2, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                return "sukses";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
         }
 
         public List<DetailLokasi> DetailLokasi()
@@ -94,6 +153,21 @@ namespace ServiceReservasi
             throw new NotImplementedException();
         }
 
+        public string Login(string username, string password)
+        {
+            string kategori = "";
+            string sql = "select Kategori from Login where Username='" + username + "' and Password='" + password + "'";
+            connection = new SqlConnection(constring);
+            com = new SqlCommand(sql, connection);
+            connection.Open();
+            SqlDataReader reader = com.ExecuteReader();
+            while (reader.Read())
+            {
+                kategori = reader.GetString(0);
+            }
+            return kategori;
+        }
+
         public string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelepon, int JumlahPemesanan, string IDLokasi)
         {
             string a = "gagal";
@@ -152,9 +226,49 @@ namespace ServiceReservasi
             return pemesanans;
         }
 
+        public string Register(string username, string password, string kategori)
+        {
+            try
+            {
+                string sql = "insert into Login values('" + username + "', '" + password + "', '" + kategori + "')";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                return "sukses";
+
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+
         public List<CekLokasi> ReviewLokasi()
         {
             throw new NotImplementedException();
+        }
+
+        public string UpdateRegister(string username, string password, string kategori, int id)
+        {
+            try
+            {
+                string sql2 = "update Login SET Username='"+username+"', Password='"+password+"', Kategori='"+kategori+"'," +
+                    "where ID_login = "+id+"";
+                connection = new SqlConnection(constring);
+                com = new SqlCommand(sql2, connection);
+                connection.Open();
+                com.ExecuteNonQuery();
+                connection.Close();
+
+                return "sukses";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
         }
 
 
